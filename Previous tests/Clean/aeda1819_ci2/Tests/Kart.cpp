1017dd1f98af10dc6a44e7a2df2cb011c2a6b6ca
jumaruba <juliane.marbayashi@gmail.com>
2019-11-19 06:44:01 +0000

    tests

---
+++ Previous tests/Clean/aeda1819_ci2/Tests/Kart.cpp
@@ -0,0 +1,98 @@
+#include <cstdlib>
+#include <iostream>
+#include <queue>
+#include <sstream>
+#include <vector>
+#include <time.h>
+#include <algorithm>
+#include <string>
+#include "Kart.h"
+
+using namespace std;
+
+//To help build better array
+string itos(int i){
+    stringstream s;
+    s << i;
+    return s.str();
+}
+
+void CGrupo::criaGrupo()
+{
+    float cilindradas[4]  = {134,250,450,600};
+    bool avariados[3]  = {true,false,false};
+
+    srand (1);   //Criar Pistas e Karts de Teste
+    for(int i=1;i<=50;i++){
+        vector<CKart> frota;
+        for(int j=1;j<=50;j++){
+            frota.push_back(CKart(avariados[rand()% 3],
+                                  ("KART - "+itos(i))+itos(j),(i-1)*50+j,cilindradas[(rand() % 4)]));
+        }
+        adicionaPista(CPista("Pista - "+itos(i),frota));
+    }
+}
+
+
+vector <CKart> CPista::getKartsAvariados()
+{
+    vector<CKart> aux;
+    for (vector<CKart>::iterator it = frotaKartsPista.begin() ; it != frotaKartsPista.end(); ++it) {
+        if ((it)->getAvariado()) aux.push_back((*it));
+    }
+    return aux;
+}
+ 
+//Exercicio 1 a)     
+vector<CKart> CGrupo::ordenaKarts()
+{
+
+	vector<CKart> vord;
+	for (int i = 0 ; i < pistasG.size(); i++ ){
+	    for (int j = 0; j < pistasG[i].getFrotaActual().size(); j++){
+	        vord.push_back(pistasG[i].getFrotaActual()[j]);
+	    }
+	}
+	sort(vord.begin(), vord.end(), [](CKart x1, CKart x2){return x1.getNumero() < x2.getNumero();});
+
+	return vord;
+    
+}
+
+//Exercicio 1 b)  
+int CGrupo::numAvariados(int cilind)
+{
+    int total = 0;
+    for (int i = 0; i < pistasG.size(); i++){
+        for (int j = 0; j < pistasG[i].getKartsAvariados().size(); j++){
+            if (pistasG[i].getKartsAvariados()[j].getCilindrada() == cilind && pistasG[i].getKartsAvariados()[j].getAvariado()) {
+                total++;
+            }
+        }
+    }
+    return total;
+}
+
+//Exercicio 1 c)   
+bool CPista::prepararCorrida(int numeroKarts, int cilind)
+{
+    int total = 0;
+    for (CKart& karts : frotaKartsPista){
+        if (cilind == karts.getCilindrada() && !karts.getAvariado()) {
+            kartsLinhaPartida.push(karts);
+            total++;
+        }
+        if (total == numeroKarts) return true;
+    }
+    for (int i = 0; i < kartsLinhaPartida.size(); i++){
+        kartsLinhaPartida.pop();
+    }
+    return false;
+}
+
+//Exercicio 1 d) 
+int CPista::inicioCorrida()
+{
+    return 0;
+}
+
